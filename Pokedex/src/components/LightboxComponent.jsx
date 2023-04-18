e96e7bd05d064343586592229784d8df74a12c99
MaartenBaeten1207 <113467320+MaartenBaeten1207@users.noreply.github.com>
2023-04-19 00:16:31 +0300

    Update LightboxComponent.jsx

--- Pokedex/src/components/LightboxComponent.jsx
+++ Pokedex/src/components/LightboxComponent.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 
-export default function LightboxComponent() {
+export default function LightboxComponent(props) {
   const [open, setOpen] = React.useState(false);
 
   return (
     <>
       <button type="button" onClick={() => setOpen(true)}>
-        Open Lightbox
+        <img className='object-contain h-60 w-96' src={props.frontDefault}></img>
       </button>
 
       <Lightbox
         open={open}
         close={() => setOpen(false)}
         slides={[
-          { src: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" }
+          { src: props.frontDefault },
+          { src: props.frontShiny },
         ]}
       />
     </>
